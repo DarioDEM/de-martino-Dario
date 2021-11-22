6d94e7396aebb6a812e5036a49befe4228dcec3e
DarioDEM <91075547+DarioDEM@users.noreply.github.com>
2021-11-22 19:26:31 +0100

    Update esempio-02.py

--- esempio-02.py
+++ esempio-02.py
@@ -1,14 +1,15 @@
-a= input("inserire un numero")
-b= input("inserire un numero2")
-c= input("inserire un numero3")
+a=(int(input("inserire numero: ")))
+b=(int(input("inserire numero: ")))
+c=(int(input("inserire numero: ")))
 
 if a>b and a>c:
-    print("il più grande è",a)
+    print('il più grande è',a)
 
 elif b>a and b>c:
-    print("il più grande è",b)
+    print('il più grande è',b)
 
 else:
-    print("il più grande è",c)
+    print('il più grande è',c)
+    
     
     
